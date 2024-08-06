05c1cb4a7f295692edb2569e4cca4239b826d29d
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-06 13:17:45 +1000

    fix sg

--- databricks/datasources.tf
+++ databricks/datasources.tf
@@ -23,12 +23,6 @@ data "azurerm_virtual_network" "this" {
   resource_group_name = var.databricks_vnet_rg_name
 }
 
-data "azurerm_subnet" "privatelink" {
-  name                 = var.databricks_privatelink_subnet
-  virtual_network_name = var.databricks_vnet_name
-  resource_group_name  = var.databricks_vnet_rg_name
-}
-
 data "azurerm_network_security_group" "public_subnet_nsg" {
   name                = var.public_subnet_nsg_id
   resource_group_name = var.databricks_vnet_rg_name
