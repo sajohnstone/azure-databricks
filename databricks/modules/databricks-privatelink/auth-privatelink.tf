a4f30450ff87dbe752e325d183becf1c5fb85e57
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-06 14:32:27 +1000

    fix typo

--- databricks/modules/databricks-privatelink/auth-privatelink.tf
+++ databricks/modules/databricks-privatelink/auth-privatelink.tf
@@ -15,7 +15,7 @@ resource "azurerm_private_endpoint" "frontend" {
 }
 
 resource "azurerm_private_dns_a_record" "databricks_ui_api" {
-  name                = "adb-${azurerm_databricks_workspace.this.workspace_id}.${var.location}.${azurerm_private_dns_zone.databricks-frontend[0].name}"
+  name                = "adb-${var.workspace_id}.${var.location}.${azurerm_private_dns_zone.databricks-frontend[0].name}"
   zone_name           = azurerm_private_dns_zone.databricks-frontend[0].name
   resource_group_name = var.resource_group_name
   ttl                 = 300
@@ -23,8 +23,8 @@ resource "azurerm_private_dns_a_record" "databricks_ui_api" {
 }
 
 resource "azurerm_private_dns_a_record" "browser_authentication" {
-  name                = "adb-${azurerm_databricks_workspace.this.workspace_id}.2.${var.location}.${azurerm_private_dns_zone.databricks-frontend[0].name}"
-  zone_name           = azurerm_private_dns_zone.databricks-frontend.name
+  name                = "adb-${var.workspace_id}.2.${var.location}.${azurerm_private_dns_zone.databricks-frontend[0].name}"
+  zone_name           = azurerm_private_dns_zone.databricks-frontend[0].name
   resource_group_name = var.resource_group_name
   ttl                 = 300
   records             = [azurerm_private_endpoint.frontend[0].private_service_connection[0].private_ip_address]
