36a8df31850ab35ee81a5617cc0b39d32c98e5e3
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-08 17:39:57 +1000

    wip

--- core/webauth.tf
+++ core/webauth.tf
@@ -58,3 +58,13 @@ resource "azurerm_private_endpoint" "webauth" {
 #   workspace_id = azurerm_databricks_workspace.webauth.workspace_id
 #   metastore_id = databricks_metastore.this.id
 # }
+
+
+resource "azurerm_private_dns_zone_virtual_network_link" "webauth" {
+  name                  = "databricks-vnetlink-backend-hub"
+  resource_group_name   = azurerm_resource_group.this.name
+  private_dns_zone_name = "privatelink.azuredatabricks.net"
+  virtual_network_id    = azurerm_virtual_network.this["hub"].id
+
+  tags = var.tags
+}
