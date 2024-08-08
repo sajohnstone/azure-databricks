13a1cc8201f8a79f7f24ab58b5dc3379f484ea96
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-08 10:16:08 +1000

    wip

--- databricks/modules/databricks-privatelink/datasources.tf
+++ databricks/modules/databricks-privatelink/datasources.tf
@@ -18,9 +18,3 @@ data "azurerm_storage_account" "this" {
   name                = var.storage_account_name
   resource_group_name = var.resource_group_name
 }
-
-data "azurerm_storage_account" "this" {
-  name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
-}
-
