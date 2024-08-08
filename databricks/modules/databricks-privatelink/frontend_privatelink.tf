0dabab087a9bd551d3d40334ca81c11222d92d1a
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-08 10:05:01 +1000

    wip

--- databricks/modules/databricks-privatelink/frontend_privatelink.tf
+++ databricks/modules/databricks-privatelink/frontend_privatelink.tf
@@ -6,7 +6,7 @@ resource "azurerm_private_endpoint" "frontend" {
 
   private_service_connection {
     name                           = "ple-${var.name}-front"
-    private_connection_resource_id = azurerm_databricks_workspace.this.id
+    private_connection_resource_id = data.azurerm_databricks_workspace.this.id
     is_manual_connection           = false
     subresource_names              = ["databricks_ui_api"]
   }
