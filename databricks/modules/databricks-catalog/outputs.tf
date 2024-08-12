9a72810933464d9c4eaf4014663fd3629fc8eeab
Stu Johnstone <sajohnstone@yahoo.co.uk>
2024-08-12 14:55:15 +1000

    wip

--- databricks/modules/databricks-catalog/outputs.tf
+++ databricks/modules/databricks-catalog/outputs.tf
@@ -1,6 +1,6 @@
 output "catalog_name" {
   description = "The ID of the resource group"
-  value       = databricks_catalog.this[0].name
+  value       = var.create_catalog == true ? databricks_catalog.this[0].name : ""
 }
 
 output "storage_account_name" {
